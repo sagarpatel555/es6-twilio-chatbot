d612a3f0311ba47fbaa3a8e6fa5ee76dfac930db
Sagar Patel <sagarpatel_555@yahoo.com>
2018-10-14 20:27:33 -0400

    12 states complete

--- Game.js
+++ Game.js
@@ -1,7 +1,16 @@
 const GameState = Object.freeze({
     WELCOMING:   Symbol("welcoming"),
-    STICK:  Symbol("stick"),
-    PLAY: Symbol("play")
+    GRAVE:   Symbol("grave"),
+    DEPOSIT:  Symbol("deposit"),
+    WITHDRAW: Symbol("withdraw"),
+    ELSE: Symbol("else"),
+    RECEIPT: Symbol("receipt"),
+    EMAIL: Symbol("email"),
+    ADDRESS: Symbol("address"),
+    QUESTION: Symbol("question"),
+    ATM: Symbol("atm"),
+    PHONE: Symbol("phone"),
+    BALANCE: Symbol("balance")
 });
 
 export default class Game{
@@ -11,26 +20,149 @@ export default class Game{
     
     makeAMove(sInput)
     {
-        let sReply = "Hi I'm spot (a dog). Oh look a stick. Do you play or keep on walking?";
+        let sReply = "Hi I'm Igor, your spooky bank chat buddy. Can I have your grave number (enter 1234)?";
         switch(this.stateCur){
             case GameState.WELCOMING:
-                this.stateCur = GameState.STICK;
+                this.stateCur = GameState.GRAVE;
                 break;
-            case GameState.STICK:
-                if(sInput.toLowerCase().match("play")){
-                    sReply = "Great my favourite game ... Here's the stick back. Do you throw it again?"
-                    this.stateCur = GameState.PLAY;
-                }else{
-                    sReply = "Walking is my favourite. Oh look a stick! Do you play or keep on walking?";
+            case GameState.GRAVE:
+                if(sInput.toLowerCase().match("1234")){
+                    sReply = "Great, would you like to deposit, withdraw, check balance or do something else?";
+                    if(sInput.toLowerCase().match("deposit")){
+                        this.stateCur = GameState.DEPOSIT;
+                    }
+                    else if(sInput.toLowerCase().match("withdraw")){
+                        this.stateCur = GameState.WITHDRAW;
+                    }
+                    else if(sInput.toLowerCase().match("balance")){
+                        this.stateCur = GameState.BALANCE;
+                    }
+                    else if(sInput.toLowerCase().match("else")){
+                        this.stateCur = GameState.ELSE;
+                    }
+                }
+                else{
+                    this.stateCur = GameState.WELCOMING;
                 }
                 break;
-            case GameState.PLAY:
+            case GameState.DEPOSIT:
+                sReply = "How much do you want to deposit?";
+                if(sInput.toLowerCase().match("/^[/d]$/")){
+                    sReply = "Do you want a receipt?";
+                    if(sInput.toLowerCase().match("yes")){
+                        this.stateCur = GameState.RECEIPT;
+                    }
+                    else{
+                        sReply = "Thank you for your business";
+                        this.stateCur = GameState.WELCOMING;
+                    }
+                }
+                else{
+                    this.stateCur = GameState.WELCOMING;
+                }
+                break;
+            case GameState.WITHDRAW:
+                sReply = "How much do you want to withdraw?";
+                if(sInput.toLowerCase().match("/^[/d]$/")){
+                    sReply = "Do you want a receipt?";
+                    if(sInput.toLowerCase().match("yes")){
+                        this.stateCur = GameState.RECEIPT;
+                    }
+                    else{
+                        sReply = "Thank you for your business";
+                        this.stateCur = GameState.WELCOMING;
+                    }
+                }
+                else{
+                    this.stateCur = GameState.WELCOMING;
+                }
+                break;
+            case GameState.BALANCE:
+                sReply = "You have 1,00,000 heads. Congratulations!!!!!";
+                this.stateCur = GameState.WELCOMING;
+                break;
+            case GameState.ELSE:
+                sReply = "Would you like to update your data?";
                 if(sInput.toLowerCase().match("yes")){
-                    sReply = "Here it is! I got it for you. Do you toss it again.... Please? ";
-                }else{
-                    sReply = "Walking is my favourite. Oh look a stick! Do you play or keep on walking?";
-                    this.stateCur = GameState.STICK;
+                    sReply = "Would you like to update your phone, email, address, security question or atm pin?";
+                    if(sInput.toLowerCase().match("phone")){
+                        this.stateCur = GameState.PHONE;
+                    }
+                    else if(sInput.toLowerCase().match("email")){
+                        this.stateCur = GameState.EMAIL;
+                    }
+                    else if(sInput.toLowerCase().match("address")){
+                        this.stateCur = GameState.ADDRESS;
+                    }
+                    else if(sInput.toLowerCase().match("question")){
+                        this.stateCur = GameState.QUESTION;
+                    }
+                    else if(sInput.toLowerCase().match("atm")){
+                        this.stateCur = GameState.ATM;
+                    }
                 }
+                else{
+                    this.stateCur = GameState.ELSE;
+                }
+                break;
+            case GameState.PHONE:
+                sReply = "Enter your phone number";
+                if(sInput.toLowerCase().match("/^[0-9]{10}$/")){
+                    sReply = "Your number has been updated";
+                    this.stateCur = GameState.WELCOMING;
+                }
+                else{
+                    sReply = "Enter correct number";
+                    this.stateCur = GameState.PHONE;
+                } 
+                break;
+            case GameState.EMAIL:
+                sReply = "Enter your email address";
+                if(sInput.toLowerCase().match("/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/")){
+                    sReply = "Your email has been updated";
+                    this.stateCur = GameState.WELCOMING;
+                }
+                else{
+                    sReply = "Enter correct email address";
+                    this.stateCur = GameState.EMAIL;
+                } 
+                break;
+            case GameState.ADDRESS:
+                sReply = "Enter your address";
+                if(sInput.toLowerCase().match("/^[/s]$/")){
+                    sReply = "Your address has been updated";
+                    this.stateCur = GameState.WELCOMING;
+                }
+                else{
+                    sReply = "Enter correct address";
+                    this.stateCur = GameState.ADDRESS;
+                } 
+                break;
+            case GameState.QUESTION:
+                sReply = "Enter your question";
+                if(sInput.toLowerCase().match("/^[/s]$/")){
+                    sReply = "Your question has been updated";
+                    this.stateCur = GameState.WELCOMING;
+                }
+                else{
+                    sReply = "Enter correct question";
+                    this.stateCur = GameState.QUESTION;
+                } 
+                break;
+            case GameState.ATM:
+                sReply = "Enter your atm pin";
+                if(sInput.toLowerCase().match("/^[/d]{4}$/")){
+                    sReply = "Your atm pin has been updated";
+                    this.stateCur = GameState.WELCOMING;
+                }
+                else{
+                    sReply = "Enter correct pin";
+                    this.stateCur = GameState.ATM;
+                }
+                break;
+            case GameState.RECEIPT:
+                sReply = "Here is your receipt";
+                this.stateCur = GameState.WELCOMING;
                 break;
         }
         return(sReply);
